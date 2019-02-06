3f340f02773698d13714a295106010a20dff0596
Ashley Renee Smith <ashleyreneesmith@zipcoders-MacBook-Pro-2.local>
2019-02-06 11:47:38 -0500

    how i made the sum of imput

--- Main.java
+++ Main.java
@@ -1,9 +1,29 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a postive Integer.");
+        
+        int input = scanner.nextInt();
+        int sum = 0;
+       for  (int i = 0; i <= input ; i++ ) {
+           sum = sum + i; 
+           
+           
+           
+        
+        
+        
+        
+        
+       
+        
     }
+   System.out.println(sum);
+}
 }
